89d618d0d3d08f89ef7dd1295e0d0dbbf1adbe9f
nitieshjaiin <158799723+nitieshjaiin@users.noreply.github.com>
2024-02-04 19:53:29 +0530

    Week 1 Task 1 & 2

---
+++ 02 Feb'24/Lab011.py
@@ -0,0 +1,9 @@
+#Task1 - Take user input for Name, Age, RollNumber and PhoneNumber and print the user details
+
+name = input("Enter your Name: ")
+age = input("Enter your Age: ")
+roll_number = input("Enter your Roll Number: ")
+phone_number = input("Enter your Phone Number: ")
+
+print("Please find the user details as below:", "\n")
+print(f"Your Name is: {name} and Your Age is: {age} and Your Roll Number is: {roll_number} and Your Phone Number is: {phone_number}")
---
+++ 02 Feb'24/Week1Task2.py
@@ -0,0 +1,12 @@
+#Print the table of 2 using print command.
+
+print(f"2 * 1 = 2")
+print(f"2 * 2 = 4")
+print(f"2 * 3 = 6")
+print(f"2 * 4 = 8")
+print(f"2 * 5 = 10")
+print(f"2 * 6 = 12")
+print(f"2 * 7 = 14")
+print(f"2 * 8 = 16")
+print(f"2 * 9 = 18")
+print(f"2 * 10 = 20")
